6e49cc189abf388ea8bcdad4b1f4538da0ca5f3e
Sandro Márcio Rosa de Souza Filho <sandror.desouza847@gmail.com>
2022-07-30 12:59:27 -0300

    Código comentado e modificando o README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# PAINT JS &#127912; #
-Link do Projeto: https://sandrords.github.io/paint-js/
+<h1> 
+<p align="center"> PAINT JS &#127912; </p>
+
+<p align="center"> 
+    <img src="images/html.png">
+    <img src="images/css.png">
+    <img src="images/javascript.png">
+</p>
+</h1>
+
+<p align="center"> 
+<a href="#descricao">Descrição</a> &bull; <a href="#ferramentas">Ferramentas Utilizadas</a>
+</p>
+
+> &#x1F517; Link do Projeto:  
+&#x27A1; https://sandrords.github.io/paint-js/
+
+> Tipo de Projeto: *Pessoal* &#x1F4DA;
+
+> Status do Projeto: &#x1F6A7; *Em Desenvolvimento* &#x1F6A7;
+
+<h2 id="descricao"> Descrição </h2>
+
+O projeto consiste na construção de uma ferramenta simples de Tela de Pintura, como Paint e Whiteboard, para navegador.   
+
+
+
+<h2 id="ferramentas"> Ferramentas Utilizadas </h2>
+
+* ***HTML*** e ***CSS***: Criação e estilização dos elementos da página.
+* ***JavaScript***: Funcionalidades e interação com a Tela de Pintura (Canvas).
+
+---
+Made by &#128153; [SandroRDS](https://www.linkedin.com/in/sandro-rosa-de-souza-02a5bb241/) &#128153; | &#x1F47E; Mais Projetos: https://github.com/SandroRDS?tab=repositories
--- js/funcoes.js
+++ js/funcoes.js
@@ -1,43 +1,50 @@
-var tela = document.querySelector("canvas");
-var desenhar = tela.getContext("2d");
+var tela          = document.querySelector("canvas");
 var paletaDeCores = document.querySelector("#cor");
-var grossura = document.querySelector("#grossura");
+var grossura      = document.querySelector("#grossura");
+var desenhar = tela.getContext("2d");
+
 var x, y;
 var pinturaAtiva = false, borracha = false;
 
-function ativarBorracha(valor)
-{
-    if(valor)
-    {
-        desenhar.fillStyle = 'lightgray';
-    }
-    else
-    {
-        desenhar.fillStyle = paletaDeCores.value;
-    }
-}
 
+//FUNÇÃO: CAPTURAR A POSIÇÃO NA TELA QUE O USUÁRIO CLICOU
 function capturarPosicao(evento)
 {
+    //ATRIBUINDO O VALOR DE X e Y COM AS COORDENADAS CAPTURADAS
     x = evento.pageX - tela.offsetLeft;
     y = evento.pageY - tela.offsetTop;
-    
+
+    //TESTANDO A CONDIÇÃO DE QUE O USUÁRIO ESTÁ COM O MOUSE PRESSIONADO
     if(pinturaAtiva)
     {
-        desenharCirculo();
+        desenharCirculo(x, y);
     }
 }
 
-function desenharCirculo()
+//FUNÇÃO: DESENHAR UM CÍRCULO NA TELA
+function desenharCirculo(x, y)
 {
     desenhar.beginPath();
     desenhar.arc(x, y, grossura.value, 0, 2*Math.PI);
     desenhar.fill();
 }
 
-function verificar()
+//FUNÇÃO: ATIVAR A BORRACHA
+function ativarBorracha(valorLogico)
 {
-    var grossura = document.querySelector("#grossura");
+    if(valorLogico) //SE borracha == TRUE -> Cor do Desenho = Cor do Painel de Fundo 
+    {
+        desenhar.fillStyle = 'lightgray';
+    }
+   else //SENÃO -> Cor do Desenho = Cor Escolhida na Paleta de Cores
+    {
+        desenhar.fillStyle = paletaDeCores.value;
+    }
+}
+
+//FUNÇÃO: VERIFICAR SE O USUÁRIO NÃO COLOCOU UM VALOR NEGATIVO NO CAMPO DE GROSSURA
+function verificarGrossura()
+{
     if(grossura.value < 1)
     {
         grossura.value = 1;
@@ -64,4 +71,4 @@ paletaDeCores.onchange = function(){
     desenhar.fillStyle = paletaDeCores.value;
 }
 
-document.getElementById("grossura").onchange = verificar;
+document.getElementById("grossura").onchange = verificarGrossura;
